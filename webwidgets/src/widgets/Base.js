314ca4607c9375c228b3de485d2fb6c4130c9244
Benjamin Yu <benjaminlyu@gmail.com>
2016-10-10 14:07:18 -0400

    FIX: Error message assumed valid responses.

--- webwidgets/src/widgets/Base.js
+++ webwidgets/src/widgets/Base.js
@@ -58,18 +58,16 @@ class Base extends Component {
   }
 
   renderError() {
-    console.log(this.state.httpResponse)
-    console.log(this.state.error)
-    return ( 
+    return (
       <div className="error_style">
          <i className="fa fa-exclamation-triangle fa-4x" aria-hidden="true"></i>
          <title>beyond the whiteboard</title>
-         <h1> Error: {this.state.error.response.statusCode} {this.state.error.response.statusText} </h1> 
-         <p>Unable to access this resource.</p> 
-         <p>Please see customer support at </p> 
+         <h1> Error: {this.state.error.message}</h1>
+         <p>Unable to access this resource.</p>
+         <p>Please see customer support at </p>
          <a href={"http://support.beyondthewhiteboard.com/"}>
          <img src={"//s3.amazonaws.com/assets.beyondthewhiteboard.com/images/btwb-logo-footer.png"} role="presentation"/>
-         </a> 
+         </a>
       </div>
     );
   }
